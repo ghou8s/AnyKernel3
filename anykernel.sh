1b19beeed47f80ab8754cbb79b44a4d207a5e580
Hanif Ardhani <hanif.ardhani98@gmail.com>
2020-11-17 23:51:52 +0800

    update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,29 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=JandaX Kernel for Redmi Note 9 Pro [joyeuse]
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
+device.name1=curtana
+device.name2=Redmi Note 9 S
+device.name3=Redmi note 9 PRO
+device.name4=xiaomi
+device.name5=RN9
+device.name6=joyeuse
+device.name7=miatoll
+device.name8=gram
+device.name9=excalibur
+device.name10=joyeuse_eea
+device.name11=Joyeuse_EEA
+supported.versions=10, 11
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -41,24 +47,17 @@ dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
 # end ramdisk changes
 
 write_boot;
 ## end install
+
+ui_print " ";
+ui_print " ";
+ui_print " JandaX Is Installed ";
+ui_print " Wet and Creamy ";
+ui_print " ";
+ui_print " Carefull!!! Curse added ";
+ui_print " ";
+ui_print " ";
 
